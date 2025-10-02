b3df54e917f2210e8dc05ce24f4dcb871e3a79ae
liuwei <zglw2012@163.com>
2025-10-02 20:33:54 +0800

    fix: 修改proxy配置

--- config/utils/proxy.js
+++ config/utils/proxy.js
@@ -1,10 +1,8 @@
-const target = "http://127.0.0.1:3000";
-
 module.exports = [
   {
-    "/api": {
-      target,
-      changeOrigin: true,
-    },
+    context: ["/api"],
+    target: "http://localhost:3000",
+    pathRewrite: { "^/api": "" },
+    secure: false,
   },
 ];
